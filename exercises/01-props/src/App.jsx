f55a45a0ed2250db830feede3e9229a216160a70
daveknussman <dknussman@yahoo.com>
2020-02-10 17:50:36 -0500

    added default parameter logic in welcome, changed call style to react component in app, capitlaized welcome function since its react component

--- exercises/01-props/src/App.jsx
+++ exercises/01-props/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
-import welcome from "./components/Welcome.jsx"
+import Welcome from "./components/Welcome.jsx"
 
 function App() {
-  return <div className="App">{welcome('Dave')}{welcome(' ')}</div>;
+  // return <div className="App">{welcome('Dave')}{welcome(' ')}</div>;
+  return <div className="App"><Welcome name='Dave'/><Welcome/></div>;
 }
 
 export default App;
--- exercises/01-props/src/components/Welcome.jsx
+++ exercises/01-props/src/components/Welcome.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const welcome = (name) => {
-    // console.log(name);
-    if (name !== ' ')
-        return <div><p>{'Welcome ' + name}</p></div>
-    else
-        return <div><p>Welcome User</p></div>;
+// const Welcome = (name) => {
+//     if (name !== ' ')
+//         return <div><p>{'Welcome ' + name}</p></div>
+//     else
+//         return <div><p>Welcome User</p></div>;
+// }
+
+const Welcome = ({name='User'}) => {
+    console.log(name);
+        return <div><p>Welcome {name}</p></div>;
 }
 
-export default welcome;
+export default Welcome;
