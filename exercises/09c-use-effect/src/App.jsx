2501307eb12fe5eb63ad50cc6080fb7151b6f613
daveknussman <dknussman@yahoo.com>
2020-03-23 15:50:51 -0400

    09c completed

--- exercises/09c-use-effect/src/App.jsx
+++ exercises/09c-use-effect/src/App.jsx
@@ -1,35 +1,83 @@
 // import useEffect
-import React, { useState } from "react";
+import React, { useState, useEffect  } from "react";
 import "./App.css";
-// import Axios (or use Fetch)
+import axios from "axios";
+
 
 function App() {
-  /**
-   * Set up state and make AJAX requests here
-   */
+
+  const [characters, setCharacters] = useState([]);
+
+  const [image, setImage] = useState("https://i.ytimg.com/vi/UFFi9PWKDjg/maxresdefault.jpg");
+
+  const [name, setName] = useState("Rick and his Friends");
+
+  useEffect(() => {
+  axios({
+    url: "https://rickandmortyapi.com/api/character",
+    method: "GET",
+    headers: {
+        "Content-Type": "applictation/json" // e.g.
+    },
+    body: JSON.stringify({ 
+    })
+  })
+  .then(response => {
+      // response.data.results.forEach((curValue)=>{
+        // dropdown.options.add( new Option(curValue.name, curValue.id));
+        setCharacters(response.data.results);
+    })  
+  .catch(() => {
+    // alert('error calling API for dropdown load!!!')
+    setName('Error calling API for dropdown!!!');
+    });
+  }, []); 
+
+  const getSelected = charId => {
+    // alert(characterId);
+    axios({
+      url: "https://rickandmortyapi.com/api/character/" + charId,
+      method: "GET",
+      headers: {
+          "Content-Type": "applictation/json" // e.g.
+      },
+      body: JSON.stringify({ 
+      })
+    })
+    .then(response => {
+      // alert('got character');
+      // console.log(response.data.name);
+      // console.log(response.data.image);
+      setImage(response.data.image);
+      setName(response.data.name);
+    })  
+    .catch(() => {
+      // alert('error calling API for get selected!!!')
+      setName('Error calling API for get selected!!!');
+      });
+  }; 
 
   return (
     <div className="container">
       <div className="row text-center" id="body">
-        <h1 id="title-head">{/* Plugin character name here */}</h1>
+        <h1 id="title-head">{name}</h1>
         <div id="main-img">
           <a href="http://rickandmorty.wikia.com/wiki/Rick_Sanchez">
             <img
-              alt={/* Plugin character name here */}
-              src={/* Plugin character image here */}
+              alt={name}
+              src={image}
               height="250"
             />
           </a>
           <div className="linkfooter">
             <p>Select your favorite character</p>
-            {/* Handle event here */}
-            <select id="dropdown" type="text">
+            <select id="dropdown" type="text" onChange={e => getSelected(e.target.value)}>
               <option></option>
-              {/**
-               * Loop through all characters. The value should be the character id.
-               * @example in HTML
-               * <option value="2" key="character-1">Morty Smith</option>
-               */}
+              {characters.map(character => (
+                <option value={character.id} key={character.id}>
+                  {character.name}
+                </option>
+              ))}
             </select>
           </div>
         </div>
