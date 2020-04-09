7de538f01b5a765677da9aaa640f9bda984448b3
daveknussman <dknussman@yahoo.com>
2020-04-08 20:15:10 -0400

    ex 13 complete

--- exercises/13-class-ajax/src/App.js
+++ exercises/13-class-ajax/src/App.js
@@ -1,13 +1,56 @@
-import React from "react";
+import React,  { Component }  from "react";
+import axios from "axios";
+// import "./App.css";
 
 /** Convert me into a class component  */
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Ron Swanson</h1>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Ron Swanson</h1>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  state = {
+    quote : 'Loading...'
+    };
+
+  componentDidMount() {
+    // could put this in a window.settimeout to test the loading message
+    axios("https://ron-swanson-quotes.herokuapp.com/v2/quotes") 
+      .then(response => {
+        this.setState({quote : response.data[0]});
+      })
+      .catch(() => {
+        this.setState({quote : 'Error loading API data'}); 
+      });
+  }
+  render () {
+    return (
+          // <div className="App">
+          //   <h1>Ron Swanson</h1>
+          //   <p>{this.state.quote}</p>
+          // </div>
+
+          <body class="bg-warning text-center">
+            <h1>Ron Swanson</h1>
+            <img src="https://res.cloudinary.com/lando726/image/upload/v1466479089/ronswanson_gkopu4.png" 
+              alt=""
+              width="450"
+              height="423"
+              class="mt-4"
+            />
+            <div class="container">
+              <blockquote id="quote" class="blockquote bg-dark text-white border-0 mb-4">
+                {this.state.quote}
+              </blockquote>
+            </div>
+          </body>
+        );
+  }
 }
+
 
 export default App;
