ff0ac081b373b1172fcb3d11c8d3133f6289c73a
daveknussman <dknussman@yahoo.com>
2020-04-20 17:52:18 -0400

    ext 15 complete

--- exercises/15-redux-react/src/components/Todo/Todo.jsx
+++ exercises/15-redux-react/src/components/Todo/Todo.jsx
@@ -14,32 +14,17 @@ import ListItem from "./ListItem/ListItem";
 
 function Todo(props) {
   const [userInput, setUserInput] = useState("");
-  // const [todos, setTodos] = useState([]);
 
-  /**
-   * This will be passed down from the container as props instead
-   * (but you still need to prevent the form from submitting and pass up user input)
-   */
-  const addTodo = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    props.setTodos([...props.todos, userInput]);
+    props.addTodo(userInput);
     setUserInput("");
   };
-  /**
-   * This will be passed down from the container as props instead
-   * (but you still need to pass up the todo index)
-   */
-  const deleteTodo = (todoIndex) => {
-    props.setTodos(
-      props.todos.filter((item, index) => {
-        return index !== todoIndex;
-      })
-    );
-  };
+
   return (
     <div className="mt-4">
       <h1 className="h3">Todo List</h1>
-      <form className="form-group" onSubmit={addTodo}>
+      <form className="form-group" onSubmit={handleSubmit}>
         <div className="input-group mb-3">
           <input
             type="text"
@@ -62,7 +47,7 @@ function Todo(props) {
         {props.todos.map((text, index) => {
           const key = `todo-item-${index}`;
           return (
-            <ListItem deleteTodo={deleteTodo} todoIndex={index} key={key}>
+            <ListItem deleteTodo={props.deleteTodo} todoIndex={index} key={key}>
               {text}
             </ListItem>
           );
