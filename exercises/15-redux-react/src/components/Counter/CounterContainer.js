d2c982f7e2a9f5659a60beb6c4e48c759e946b71
daveknussman <dknussman@yahoo.com>
2020-04-20 15:57:30 -0400

    latest changes for ex15

--- exercises/15-redux-react/src/components/Counter/CounterContainer.js
+++ exercises/15-redux-react/src/components/Counter/CounterContainer.js
@@ -7,7 +7,6 @@
 /**
  * Import something form Redux here
  */
-<<<<<<< HEAD
 import { connect } from "react-redux";
 
 import Counter from "./Counter";
@@ -24,23 +23,10 @@ function mapStateToProps(state) {
         count: state.count
       };
 }
-=======
-import Counter from "./Counter";
-
-/**
- * Import the actions that you need
- */
 
 /**
  * Complete this function. You may need to pass in arguements
  */
-function mapStateToProps() {}
->>>>>>> 40892a105b1f14e293b4ffba867e2c03f2f69716
-
-/**
- * Complete this function. You may need to pass in arguements
- */
-<<<<<<< HEAD
 function mapDispatchToProps(dispatch) {
     // console.log("dispatch", dispatch);
     return {
@@ -48,18 +34,11 @@ function mapDispatchToProps(dispatch) {
         decreaseCount: () => dispatch(decreaseCount())
       };
 }
-=======
-function mapDispatchToProps() {}
->>>>>>> 40892a105b1f14e293b4ffba867e2c03f2f69716
 
 /**
  * Refactor this so that you are connecting the Counter to the Redux store.
  */
-<<<<<<< HEAD
 export default connect(
     mapStateToProps,
     mapDispatchToProps
   )(Counter);
-=======
-export default Counter;
->>>>>>> 40892a105b1f14e293b4ffba867e2c03f2f69716
